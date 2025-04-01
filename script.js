b45974a63604a8c27e5ebfb85ca4a5956704f90b
akash08884 <akash08884@gmail.com>
2025-04-02 03:16:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,45 @@
-$(document).ready(function() {
-    let message = "Happy Birthday! May your day be filled with joy and laughter!";
-    let currentIndex = 0;
+document.addEventListener('DOMContentLoaded', () => {
+    const birthdaySong = document.getElementById('birthdaySong');
+    const cheerSound = document.getElementById('cheerSound');
+    const btnCelebrate = document.getElementById('btn__celebrate');
+    const fireworksCanvas = document.getElementById('fireworks');
+    const ctx = fireworksCanvas.getContext('2d');
 
-    // Play Birthday song and display message
-    $("#btn__letter").click(function() {
-        document.getElementById("birthdaySong").play();
-        let interval = setInterval(function() {
-            if (currentIndex < message.length) {
-                $(".message").append(message[currentIndex]);
-                currentIndex++;
-            } else {
-                clearInterval(interval);
-            }
-        }, 100);
-    });
-
-    // Blow Candles Action
-    $("#blowCandles").click(function() {
-        $("#cakeImg").attr("src", "images/cake-blown.png");
-        document.getElementById("cheerSound").play();
-    });
-
-    // Fireworks Effect
-    function createFireworks() {
-        const canvas = document.getElementById("fireworks");
-        const ctx = canvas.getContext("2d");
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-
-        function drawFirework(x, y) {
-            for (let i = 0; i < 100; i++) {
-                setTimeout(() => {
-                    ctx.beginPath();
-                    ctx.arc(x + Math.random() * 100 - 50, y + Math.random() * 100 - 50, 2, 0, Math.PI * 2);
-                    ctx.fillStyle = `hsl(${Math.random() * 360}, 100%, 50%)`;
-                    ctx.fill();
-                }, i * 10);
-            }
-        }
-
-        setInterval(() => {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            drawFirework(Math.random() * canvas.width, Math.random() * canvas.height / 2);
-        }, 1000);
+    // Resize canvas to fill window
+    function resizeCanvas() {
+        fireworksCanvas.width = window.innerWidth;
+        fireworksCanvas.height = window.innerHeight;
     }
-    createFireworks();
-});
+    window.addEventListener('resize', resizeCanvas);
+    resizeCanvas();
+
+    // Fireworks Animation
+    function createFireworks() {
+        const particles = [];
+        function Particle(x, y) {
+            this.x = x;
+            this.y = y;
+            this.size = Math.random() * 3 + 1;
+            this.speedX = Math.random() * 6 - 3;
+            this.speedY = Math.random() * 6 - 3;
+            this.color = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        }
+        Particle.prototype.update = function() {
+            this.x += this.speedX;
+            this.y += this.speedY;
+            this.size *= 0.96;
+        };
+        Particle.prototype.draw = function() {
+            ctx.fillStyle = this.color;
+            ctx.strokeStyle = this.color;
+            ctx.lineWidth = 2;
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+            ctx.fill();
+            ctx.stroke();
+        };
+        function animate() {
+            ctx.clearRect(0, 0, fireworksCanvas.width, fireworksCanvas.height);
+            particles.forEach((particle, index) => {
+                particle.update();
+               4
